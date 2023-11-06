50ba8e373cf242caee3030edb8787e7f9923db2f
RubenMansilla <rumansillar@gmail.com>
2023-11-06 09:03:54 +0100

    adding colors

--- src/aventuraConversacional/Dialogo.java
+++ src/aventuraConversacional/Dialogo.java
@@ -10,6 +10,9 @@ public class Dialogo {
 
 	public static final String ANSI_GREEN = "\u001B[32m";
 	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
 
 	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 

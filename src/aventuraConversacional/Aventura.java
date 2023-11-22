d1785aa40f59569ba7d7ca593950692aec9b945e
Ángel Garcia Beltrán <angelurano00@gmail.com>
2023-11-22 12:21:08 +0100

    Indent correctly all files

--- src/aventuraConversacional/Aventura.java
+++ src/aventuraConversacional/Aventura.java
@@ -8,7 +8,7 @@ public class Aventura {
 	public static void aIntro() {
 		// TODO
 	}
-	
+
 	public static String a001() {
 		System.out.println(Dialogo.d001());
 		String nombre = sc.nextLine();
@@ -18,26 +18,26 @@ public class Aventura {
 		}
 		return nombre;
 	}
-	
+
 	public static int a002() {
-		//TODO
+		// TODO
 		System.out.println(Dialogo.d003());
 		System.out.println(Dialogo.m002());
 		int opcion = sc.nextInt();
-		if (opcion==1) {
-			
-		}else if(opcion==2) {
-			
-		}else if (opcion==3) {
-			
+		if (opcion == 1) {
+
+		} else if (opcion == 2) {
+
+		} else if (opcion == 3) {
+
 		}
-		while (opcion < 1 || opcion >3) {
+		while (opcion < 1 || opcion > 3) {
 			System.out.println(Dialogo.e002());
 			opcion = sc.nextInt();
 		}
 		return opcion;
 	}
-	
+
 	public static void fin() {
 		sc.close();
 	}
--- src/aventuraConversacional/Main.java
+++ src/aventuraConversacional/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		Aventura.aIntro();
 		String nombre = Aventura.a001();
-		
+
 		int opcion = Aventura.a002();
-		
+
 		Aventura.fin();
 	}
 

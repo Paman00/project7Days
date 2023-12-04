0c86ae3da5aff6c66599b04de522acd99439b5fd
kiwan33 <rumansillar@gmail.com>
2023-12-04 11:48:49 +0100

    Methodes for MC, storyteller and other charecters. Dialogues addes

--- src/aventuraConversacional/Dialogo.java
+++ src/aventuraConversacional/Dialogo.java
@@ -7,7 +7,7 @@ public class Dialogo {
 		return FuncionesDialogo.agregarColor(textoEnCaja, Recursos.BLACK_BACKGROUND + Recursos.PURPLE);
 	}
 
-	public static String modeloCajaBasico(String[] texto) {
+	public static String modeloCajaProta(String[] texto) {
 		String textoEnCaja = FuncionesDialogo.formatearTextoCaja(texto);
 		return FuncionesDialogo.agregarColor(textoEnCaja, Recursos.BLACK_BACKGROUND + Recursos.GREEN);
 	}
@@ -15,6 +15,16 @@ public class Dialogo {
 	public static String modeloCajaError(String[] texto) {
 		String textoEnCaja = FuncionesDialogo.formatearTextoCajaPersonalizada(texto, '=', '!');
 		return FuncionesDialogo.agregarColor(textoEnCaja, Recursos.BLACK_BACKGROUND + Recursos.RED);
+	}
+
+	public static String modeloCajaOtrosPersonajes(String[] texto) {
+		String textoEnCaja = FuncionesDialogo.formatearTextoCajaPersonalizada(texto, '*', '*');
+		return FuncionesDialogo.agregarColor(textoEnCaja, Recursos.BLACK_BACKGROUND + Recursos.CYAN);
+	}
+
+	public static String modeloCajaNarrador(String[] texto) {
+		String textoEnCaja = FuncionesDialogo.formatearTextoCajaPersonalizada(texto, '=', '|');
+		return FuncionesDialogo.agregarColor(textoEnCaja, Recursos.BLACK_BACKGROUND + Recursos.PURPLE);
 	}
 
 	/* Cajas de Texto */
@@ -27,7 +37,7 @@ public class Dialogo {
 	public static String cajaInicio() {
 		String[] textoParaCaja = { "Año 2026",
 				"Hoy por fin es el día de la misión, el ser humano no pisa la luna desde el 72" };
-		return modeloCajaBasico(textoParaCaja);
+		return modeloCajaNarrador(textoParaCaja);
 	}
 
 	public static String cajaErrorInicio() {
@@ -38,7 +48,7 @@ public class Dialogo {
 	public static String cajaSeleccionarObjeto(String nombre) {
 		String[] textoParaCaja = { nombre + ", ",
 				"vas a estar una semana fuera, 3 días de ida, uno en la luna y 3 de vuelta" };
-		return modeloCajaBasico(textoParaCaja);
+		return modeloCajaNarrador(textoParaCaja);
 	}
 
 	public static String cajaErrorSeleccionarObjeto() {
@@ -48,18 +58,28 @@ public class Dialogo {
 
 	public static String cajaSeleccionarModoSalida() {
 		String[] textoParaCaja = { "05:35", "Coges tus cosas, te metes en el coche y vas al lanzamiento" };
-		return modeloCajaBasico(textoParaCaja);
+		return modeloCajaNarrador(textoParaCaja);
 	}
 
 	public static String cajaLanzamiento() {
 		String[] textoParaCaja = { "Te preparas para el lanzamiento, antes de subir a la nave,",
-				"tienes que pasar un test rápido de repaso, eres un experto 	así que no es un problema para ti" };
-		return modeloCajaBasico(textoParaCaja);
+				"tienes que pasar un test rápido de repaso, eres un experto 4así que no es un problema para ti" };
+		return modeloCajaNarrador(textoParaCaja);
 	}
 
 	public static String cajaErrorSeleccionarModo() {
 		String[] textoParaCaja = { "Debe de elegir un numero:", "1 o 2" };
 		return modeloCajaError(textoParaCaja);
+	}
+
+	public static String cajaHoraLLegada01() {
+		String[] textoParaCaja = { "Llegas a las 06:25 al lanzamiento" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaHoraLLegada02() {
+		String[] textoParaCaja = { "Llegas a las 06:00 al lanzamiento" };
+		return modeloCajaNarrador(textoParaCaja);
 	}
 
 	public static String cajaFallorespeto1() {
@@ -68,8 +88,139 @@ public class Dialogo {
 		return modeloCajaError(textoParaCaja);
 	}
 
+	public static String cajaJefe1() {
+		String[] textoParaCaja = {
+				"Eso, la fuerza de la gravedad hace que se queme mucho combustible para poder salir" };
+		return modeloCajaOtrosPersonajes(textoParaCaja);
+	}
+
+	public static String cajaJefe2() {
+		String[] textoParaCaja = { "¿Estás bien? ¿Sabes por lo menos a dónde vas no?" };
+		return modeloCajaOtrosPersonajes(textoParaCaja);
+	}
+
 	public static String cajaErrorRespeto1() {
 		String[] textoParaCaja = { "Debe de elegir un numero:", "1, 2, 3, 4, 5 o 6" };
 		return modeloCajaError(textoParaCaja);
+	}
+
+	public static String cajaProtaRespuestaRespeto01() {
+		String[] textoParaCaja = { "...",
+				"No puede ser, llevo toda la vida esperando para esto y se me ha olvidado que es lo que iba a hacer" };
+		return modeloCajaProta(textoParaCaja);
+	}
+
+	public static String cajaNarrador01() {
+		String[] textoParaCaja = { "...",
+				"No puede ser, llevo toda la vida esperando para esto y se me ha olvidado que es lo que iba a hacer" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaNarrador02() {
+		String[] textoParaCaja = { "(Ibas a responder pero de repente la respuesta se esfumó de tu mente)" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaNarrador03() {
+		String[] textoParaCaja = { "(No respondes)" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaProtaRespuestaRespeto02() {
+		String[] textoParaCaja = { "(Venga, eso por lo menos tengo que saberlo" };
+		return modeloCajaProta(textoParaCaja);
+	}
+
+	public static String cajaErrorRespeto2() {
+		String[] textoParaCaja = { "Debe de elegir un numero:", "1, 2, 3, 4, 5, 6, 7 o 8" };
+		return modeloCajaError(textoParaCaja);
+	}
+
+	public static String cajaFallorespeto2() {
+		String[] textoParaCaja = { "No, no vas allí señor, ¿Seguro que está todo bien?" };
+		return modeloCajaError(textoParaCaja);
+	}
+
+	public static String cajaJefe3() {
+		String[] textoParaCaja = { "Jajaja, Claro que sí, ya verás cómo sale todo bien" };
+		return modeloCajaOtrosPersonajes(textoParaCaja);
+	}
+
+	public static String cajaJefe4(String objetoInventario, String[] inventario) {
+		String[] textoParaCaja = { "Eh... señor esto no es un, " + objetoInventario + ", es un " + inventario[0] };
+		return modeloCajaOtrosPersonajes(textoParaCaja);
+	}
+
+	public static String cajaJefeMuerteRespeto1() {
+		String[] textoParaCaja = { "Vale, espera aquí un momento" };
+		return modeloCajaOtrosPersonajes(textoParaCaja);
+	}
+
+	public static String cajaJefeMuerteRespeto2(String nombre) {
+		String[] textoParaCaja = { nombre + " hemos estado hablando y creemos que va a ser mejor",
+				"que hoy no subas a la nave, deberías irte a casa a descansar" };
+		return modeloCajaOtrosPersonajes(textoParaCaja);
+	}
+
+	public static String cajaMuerteNarrador() {
+		String[] textoParaCaja = { "(Se han ido sin ti)" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaNarrador04() {
+		String[] textoParaCaja = { "Todo correcto, queda poco para subir a la nave, ¿Quieres revisar algo?" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaNarrador05(String[] inventario) {
+		String[] textoParaCaja = { "Solo llevas" + inventario[0] + ", igual es poco,",
+				"¿Quieres buscar algo más que llevarte?" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaNarrador06() {
+		String[] textoParaCaja = { "Encuentras las llaves del coche de tu jefe y te las llevas" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaNarrador07() {
+		String[] textoParaCaja = { "¿Está al 75%, quieres avisar de esto?" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+
+	public static String cajaNarrador08() {
+		String[] textoParaCaja = { "Ooh, ya, no lo hemos llenado del todo para que no pese más",
+				" y no use tanto combustible para salir del agujero gravitational de la tierra" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+	
+	public static String cajaNarrador09() {
+		String[] textoParaCaja = { "Pero si es verdad que está más bajo de lo que recordaba… Lo llenaremos hasta el 85%.", " Gracias." };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+	
+	public static String cajaNarrador10(String nombre) {
+		String[] textoParaCaja = { nombre + ", ¿quieres que repasemos el plan?", "Perfecto" };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+	
+	public static String cajaNarrador11() {
+		String[] textoParaCaja = {"Vamos a ir a explorar la luna por primera vez desde que la extraña nube de polvo la envolvió hace 2 años, desde entonces hemos perdido toda pista de qué está pasando allí, todos los rovers que hemos mandado acaban con la cámara totalmente tapada, necesitamos que alguien vaya allí en persona para ver qué está pasando y de dónde viene ese polvoS"};
+		return modeloCajaNarrador(textoParaCaja);
+	}
+	
+	public static String cajaNarrador12() {
+		String[] textoParaCaja = { "El viaje de ida son 72 horas, estarás en la luna unas 12 horas, y harás otras 72 horas de vuelta. Sabemos que eres capaz." };
+		return modeloCajaNarrador(textoParaCaja);
+	}
+	
+	public static String cajaNarrador13() {
+		String[] textoParaCaja = {"Hora de subir pues", "(te pones el traje, subes a la nave y te preparas para el despegue)"};
+		return modeloCajaNarrador(textoParaCaja);
+	}
+	
+	public static String cajaNarrador14() {
+		String[] textoParaCaja = {"Ya son las 7am, la ventana de lanzamiento abre a las 7:30am", "¿Qué vas a hacer mientras esperas?"};
+		return modeloCajaNarrador(textoParaCaja);
 	}
 }
--- src/aventuraConversacional/Main.java
+++ src/aventuraConversacional/Main.java
@@ -17,6 +17,8 @@ public class Main {
 		int respeto = 5;
 		int contadorFallo = 0;
 		String objetoInventario = null;
+		boolean objetoEncontrado = false;
+		boolean finDeLaPartida = false;
 
 		// Damos la introducción al usuario y esperamos a que presione START
 		System.out.println(Dialogo.cajaIntroduccion());
@@ -112,20 +114,21 @@ public class Main {
 
 		System.out.println(Recursos.naveAntesDeDespegar);
 		if (opcion == 1) {
-			System.out.println(centrarLinea("Llegas a las 06:00"));
+			System.out.println(Dialogo.cajaHoraLLegada01());
 		} else if (opcion == 2) {
-			System.out.println(centrarLinea("Llegas a las 06:25"));
+			System.out.println(Dialogo.cajaHoraLLegada02());
 		}
-		System.out.println(centrarLinea("Llegas al lanzamiento"));
 		System.out.println(centrarLinea("Presione START para continuar"));
 		sc.nextLine();
 
 		System.out.println(Dialogo.cajaLanzamiento());
 		System.out.println(centrarLinea("Presione START para continuar"));
 		sc.nextLine();
+
 		// Reiniciamos el valor de la variable usada para escoger las opciones
 		opcion = 0;
-		System.out.println(centrarLinea("¿En qué parte se gasta más combustible?"));
+
+		System.out.println(agregarColor(centrarLinea("\n¿En qué parte se gasta más combustible?"), Recursos.PURPLE));
 		System.out.println("1. En la salida de la atmósfera de la Tierra");
 		System.out.println("2. En el espacio");
 		System.out.println("3. En el aterrizaje en la Luna ");
@@ -136,7 +139,6 @@ public class Main {
 
 		while (opcion < 1 || opcion > 5) {
 			System.out.println(Dialogo.cajaErrorRespeto1());
-			System.out.println(centrarLinea("Ingrese 1, 2, 3, 4 o 5 para elegir la respuesta"));
 			opcion = sc.nextInt();
 			contadorFallo++;
 			if (contadorFallo == 1) {
@@ -148,15 +150,21 @@ public class Main {
 			System.out.println(Dialogo.cajaFallorespeto1());
 			System.out.println(centrarLinea("Ingrese 1, 2, 3, 4 o 5 para elegir la respuesta"));
 			opcion = sc.nextInt();
+			contadorFallo++;
+			if (contadorFallo == 1) {
+				respeto--;
+			}
 		}
 
-		System.out.println(
-				centrarTexto("Eso, la fuerza de la gravedad hace que se queme mucho combustible para poder salir"));
+		System.out.println("El respeto es de " + respeto);
+
+		System.out.println(Dialogo.cajaJefe1());
 
 		// Reiniciamos el valor de la variable usada para escoger las opciones
 		opcion = 0;
+		contadorFallo = 0;
 
-		System.out.println(centrarLinea("¿Llevas algún objeto que declarar?"));
+		System.out.println(agregarColor(centrarLinea("\n¿Llevas algún objeto que declarar?"), Recursos.PURPLE));
 		System.out.println("1. Bola de Nieve de Cristal");
 		System.out.println("2. Destornillador");
 		System.out.println("3. Pájaro de Madera");
@@ -170,27 +178,133 @@ public class Main {
 			objetoInventario = "Destornillador";
 		} else if (opcion == 3) {
 			objetoInventario = "Pájaro de Madera";
+		} else if (opcion == 4) {
+			objetoInventario = "No";
 		}
-
-		boolean objetoEncontrado = false;
 
 		for (int i = 0; i < inventario.length; i++) {
 			if (objetoInventario.equals(inventario[i])) {
 				objetoEncontrado = true;
-				System.out.println(centrarLinea("(Revisa tu equipaje)"));
-				System.out.println(centrarLinea("Efectivamente, un " + inventario[i] + " todo correcto"));
+				System.out.println(agregarColor(centrarLinea("\nRevisan tu equipaje"), Recursos.PURPLE));
+				System.out.println(agregarColor(centrarLinea("\nEfectivamente, un " + inventario[0] + " todo correcto"),
+						Recursos.CYAN));
 				i = inventario.length; // Si encuentra el objeto, sale del bucle
 			}
 		}
 		if (!objetoEncontrado) {
-			System.out.println(centrarLinea("(Revisa tu equipaje)"));
-			System.out
-					.println(centrarLinea("Eh… señor esto no es un " + objetoInventario + ", es un " + inventario[0]));
+			System.out.println(agregarColor(centrarLinea("\nRevisan tu equipaje"), Recursos.PURPLE));
+			System.out.println(Dialogo.cajaJefe4(objetoInventario, inventario));
+
 			respeto--;
 		}
 
-		System.out.println(
-				agregarColor(centrarLinea("Proxima pregunta, ¿por qué estamos haciendo esta misión?"), Recursos.RED));
+		System.out.println("El respeto es de " + respeto);
+
+		System.out.println(agregarColor(centrarLinea("\nProxima pregunta, ¿por qué estamos haciendo esta misión?"),
+				Recursos.PURPLE));
+		System.out.println(Dialogo.cajaNarrador02());
+		System.out.println(centrarLinea("Presione START para continuar"));
+		sc.nextLine();
+		System.out.println(Dialogo.cajaProtaRespuestaRespeto01());
+		System.out.println(centrarLinea("Presione START para continuar"));
+		sc.nextLine();
+		System.out.println(Dialogo.cajaNarrador03());
+		System.out.println(centrarLinea("Presione START para continuar"));
+		sc.nextLine();
+		System.out.println(Dialogo.cajaJefe2());
+		System.out.println(centrarLinea("Presione START para continuar"));
+		sc.nextLine();
+		System.out.println(Dialogo.cajaProtaRespuestaRespeto02());
+
+		// Reiniciamos el valor de la variable usada para escoger las opciones
+		opcion = 0;
+		contadorFallo = 0;
+
+		System.out.println("\n1. Marte");
+		System.out.println("2. Saturno");
+		System.out.println("3. Urano");
+		System.out.println("4. La Luna");
+		System.out.println("5. Plutón");
+		System.out.println("6. El sol");
+		System.out.println("7. Bogotá");
+		System.out.println("8. Ferrol");
+		System.out.println(centrarLinea("Ingrese 1, 2, 3, 4, 5, 6, 7 o 8 para elegir una ubucación"));
+		opcion = sc.nextInt();
+
+		while (opcion < 1 || opcion > 8) {
+			System.out.println(Dialogo.cajaErrorRespeto2());
+			opcion = sc.nextInt();
+			contadorFallo++;
+			if (contadorFallo == 1) {
+				respeto--;
+			}
+		}
+
+		while (opcion != 4) {
+			System.out.println(Dialogo.cajaFallorespeto2());
+			opcion = sc.nextInt();
+			contadorFallo++;
+			if (contadorFallo == 1) {
+				respeto--;
+
+			}
+		}
+
+		System.out.println(Dialogo.cajaJefe3());
+
+		// TODO
+		/*
+		 * if (respeto <= 0) { System.out.println(Dialogo.cajaJefeMuerteRespeto1());
+		 * System.out.println(Dialogo.cajaJefeMuerteRespeto2(nombre));
+		 * System.out.println(Recursos.despegue);
+		 * System.out.println(Dialogo.cajaMuerteNarrador()); finDeLaPartida = true; }
+		 */
+
+		System.out.println(centrarLinea("Presione START para continuar"));
+		sc.nextLine();
+
+		System.out.println(Dialogo.cajaNarrador04());
+
+		// Reiniciamos el valor de la variable usada para escoger las opciones
+		opcion = 0;
+
+		while (opcion != 4) {
+			System.out.println("\n1. Revisar inventario");
+			System.out.println("2. Revisar combustible");
+			System.out.println("3. Revisar el plan con tu jefe");
+			System.out.println("4. No");
+			System.out.println(centrarLinea("Ingrese 1, 2, 3, 4, 5, 6, 7 o 8 para elegir una ubucación"));
+			opcion = sc.nextInt();
+
+			if (opcion == 1) {
+				opcion = 0;
+				System.out.println(Dialogo.cajaNarrador05(inventario));
+				System.out.println("1. Si");
+				System.out.println("2. No");
+				opcion = sc.nextInt();
+				if (opcion == 1) {
+					System.out.println(Dialogo.cajaNarrador06());
+					System.out.println(Recursos.llaves);
+				}
+			} else if (opcion == 2) {
+				opcion = 0;
+				System.out.println(Dialogo.cajaNarrador07());
+				System.out.println("1. Si");
+				System.out.println("2. No");
+				opcion = sc.nextInt();
+				if (opcion == 1) {
+					System.out.println(Dialogo.cajaNarrador08());
+					System.out.println(Dialogo.cajaNarrador09());
+				}
+			} else if (opcion == 3) {
+				System.out.println(Dialogo.cajaNarrador10(nombre));
+				System.out.println(Dialogo.cajaNarrador11());
+				System.out.println(Dialogo.cajaNarrador12());
+
+			}
+		}
+
+		System.out.println("hskfsdofjsdfoiñsnd");
 
 	}
 
